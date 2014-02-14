d8b1f97c8e7d0fdf8fe8d9fd99f3fc2c68cb1522
Sergiy Misyura <sergey@cando.com>
2014-02-14 14:29:00 -0500

    Working on the front page

--- website-new/protected/views/main.php
+++ website-new/protected/views/main.php
@@ -3,6 +3,32 @@
 <head>
 </head>
 <body>
-<?php echo $content ?>
+	<div class="navbar navbar-default">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand">
+					<img class="navbar-logo" src="<?= Yii::app()->baseUrl ?>/assets/img/poemz_logo.png" />
+				</a>
+			</div>
+			<div class="collapse navbar-collapse">
+				<form class="navbar-form navbar-right">
+					<button type="button" class="btn btn-primary">Sign in</button>
+				</form>
+				<form class="navbar-form navbar-right">
+					<div class="form-group">
+						<input type="search" class="form-control" placeholder="Find your favorite poem" />
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+	<?php echo $content ?>
 </body>
 </html>
--- website-new/protected/views/site/index.php
+++ website-new/protected/views/site/index.php
@@ -1,1 +1,16 @@
-test
+<div class="container front-page">
+	<h2 class="tag-line text-center text-primary">Let the world hear your favorite poem...</h2>
+	<h4 class="tag-line text-center">and hear others' interpretations of your favorites.</h4>
+
+	<ul class="nav nav-tabs">
+		<li class="active"><a href="#">Best stuff ever</a></li>
+		<li><a href="#">Yours favorite</a></li>
+		<li><a href="#">Random</a></li>
+	</ul>
+
+	<div class="row">
+		<div class="col-sm-2">
+			<div style="background-color: grey; width:100%; height: 140px;"></div>
+		</div>
+	</div>
+</div>
