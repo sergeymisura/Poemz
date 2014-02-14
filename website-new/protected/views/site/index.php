f77f70f83cbd6eb198481017632c5eb20b22f35b
Sergiy Misyura <sergey@cando.com>
2014-02-14 17:44:56 -0500

    Front page mocks

--- website-new/protected/views/site/index.php
+++ website-new/protected/views/site/index.php
@@ -4,13 +4,112 @@
 
 	<ul class="nav nav-tabs">
 		<li class="active"><a href="#">Best stuff ever</a></li>
-		<li><a href="#">Yours favorite</a></li>
+		<li><a href="#">Your favorites</a></li>
 		<li><a href="#">Random</a></li>
 	</ul>
 
+	<br/>
+
 	<div class="row">
 		<div class="col-sm-2">
-			<div style="background-color: grey; width:100%; height: 140px;"></div>
+			<div><img src="<?= Yii::app()->baseUrl ?>/assets/img/portrait_placeholder.jpg" class="img-responsive img-thumbnail" /></div>
+			<div class="poem-info">
+				<h5 class="title text-primary"><a href="#">Sonnet XX</a></h5>
+				<div>by <a href="#">William Shakespeare</a></div>
+				<div class="first-line">A woman's face with Nature's own hand painted...</div>
+			</div>
+			<div class="read-by">
+				<div>#1 read by <a href="#">robratterman</a></div>
+				<div>#2 read by <a href="#">bobert</a></div>
+			</div>
+			<h6 class="text-center"><a href="#">Hear more versions</a></h6>
+			<div><button class="btn btn-primary btn-xs record-own">Record your version</button> </div>
+		</div>
+		<div class="col-sm-2">
+			<div><img src="<?= Yii::app()->baseUrl ?>/assets/img/portrait_placeholder_2.jpg" class="img-responsive img-thumbnail" /></div>
+			<div class="poem-info">
+				<h5 class="title text-primary"><a href="#">Tenderness</a></h5>
+				<div>by <a href="#">Marina Tsvetaeva</a></div>
+				<div class="first-line">Where does this tenderness come from...</div>
+			</div>
+			<div class="read-by">
+				<div>#1 read by <a href="#">robratterman</a></div>
+				<div>#2 read by <a href="#">bobert</a></div>
+			</div>
+			<h6 class="text-center"><a href="#">Hear more versions</a></h6>
+			<div><button class="btn btn-primary btn-xs record-own">Record your version</button> </div>
+		</div>
+		<div class="col-sm-2">
+			<div><img src="<?= Yii::app()->baseUrl ?>/assets/img/portrait_placeholder_3.jpg" class="img-responsive img-thumbnail" /></div>
+			<div class="poem-info">
+				<h5 class="title text-primary"><a href="#">The Unknown Citizen</a></h5>
+				<div>by <a href="#">Wystan Hugh Auden</a></div>
+				<div class="first-line">He was found by the Bureau of Statistics to be...</div>
+			</div>
+			<div class="read-by">
+				<div>#1 read by <a href="#">robratterman</a></div>
+				<div>#2 read by <a href="#">bobert</a></div>
+			</div>
+			<h6 class="text-center"><a href="#">Hear more versions</a></h6>
+			<div><button class="btn btn-primary btn-xs record-own">Record your version</button> </div>
+		</div>
+		<div class="col-sm-2">
+			<div><img src="<?= Yii::app()->baseUrl ?>/assets/img/portrait_placeholder_4.jpg" class="img-responsive img-thumbnail" /></div>
+			<div class="poem-info">
+				<h5 class="title text-primary"><a href="#">A Bard's Epitaph</a></h5>
+				<div>by <a href="#">Robert Burns</a></div>
+				<div class="first-line">Is there a whim-inspired fool...</div>
+			</div>
+			<div class="read-by">
+				<div>#1 read by <a href="#">robratterman</a></div>
+				<div>#2 read by <a href="#">bobert</a></div>
+			</div>
+			<h6 class="text-center"><a href="#">Hear more versions</a></h6>
+			<div><button class="btn btn-primary btn-xs record-own">Record your version</button> </div>
+		</div>
+		<div class="col-sm-2">
+			<div><img src="<?= Yii::app()->baseUrl ?>/assets/img/portrait_placeholder_5.jpg" class="img-responsive img-thumbnail" /></div>
+			<div class="poem-info">
+				<h5 class="title text-primary"><a href="#">Odysseus to Telemachus</a></h5>
+				<div>by <a href="#">Joseph Brodsky</a></div>
+				<div class="first-line">My dear Telemachus,	the Trojan War is over now; I don't recall who won it...</div>
+			</div>
+			<div class="read-by">
+				<div>#1 read by <a href="#">robratterman</a></div>
+				<div>#2 read by <a href="#">bobert</a></div>
+			</div>
+			<h6 class="text-center"><a href="#">Hear more versions</a></h6>
+			<div><button class="btn btn-primary btn-xs record-own">Record your version</button> </div>
+		</div>
+		<div class="col-sm-2">
+			<div><img src="<?= Yii::app()->baseUrl ?>/assets/img/portrait_placeholder_6.jpg" class="img-responsive img-thumbnail" /></div>
+			<div class="poem-info">
+				<h5 class="title text-primary"><a href="#">Haunted Houses</a></h5>
+				<div>by <a href="#">Henry Wadsworth Longfellow</a></div>
+				<div class="first-line">All houses wherein men have lived and died...</div>
+			</div>
+			<div class="read-by">
+				<div>#1 read by <a href="#">robratterman</a></div>
+				<div>#2 read by <a href="#">bobert</a></div>
+			</div>
+			<h6 class="text-center"><a href="#">Hear more versions</a></h6>
+			<div><button class="btn btn-primary btn-xs record-own">Record your version</button> </div>
 		</div>
 	</div>
+
+	<br/>
+
+	<div class="more">
+		<span class="pull-right">more</span>
+	</div>
+
+	<br/>
+	<br/>
+
+	<div class="text-center">
+		<button type="button" class="btn btn-primary btn-lg record-now">Record your favorite poem now!</button>
+	</div>
+
+	<br/>
+	<br/>
 </div>
