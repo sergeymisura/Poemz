47b94626dd15ce15cde4f64a61605acda2e1e5b6
Sergiy Misyura <sergey@cando.com>
2014-03-09 16:49:27 -0400

    Now playing, Rob's picture

--- website-new/protected/views/site/poem.php
+++ website-new/protected/views/site/poem.php
@@ -23,6 +23,20 @@
 			</div>
 		</div>
 		<div class="col-sm-6">
+			<div class="well now-playing">
+				<h4>Now playing</h4>
+				<div  class="row">
+					<div class="col-sm-3">
+						<img class="thumbnail" src="<?= Yii::app()->baseUrl ?>/assets/img/portrait_placeholder_3.jpg" />
+					</div>
+					<div class="col-sm-9 recited-by">
+						<div>recited by</div>
+						<h4><a href="#">robratterman</a></h4>
+					</div>
+				</div>
+				<div><img src="<?= Yii::app()->baseUrl ?>/assets/img/player_stub.png" width="100%" height="30px" /></div>
+			</div>
+
 			<h4 class="recitations-header">poemz recitations</h4>
 			<div class="recitations">
 				<div class="row">
@@ -41,7 +55,10 @@
 									<nobr><img src="<?= Yii::app()->baseUrl ?>/assets/img/thumb_up.png"/> 42 votes</nobr>
 								</div>
 								<div class="col-sm-6">
-									<nobr><img src="<?= Yii::app()->baseUrl ?>/assets/img/comments_sm.png"/> 30 comments</nobr>
+									<nobr>
+										<img src="<?= Yii::app()->baseUrl ?>/assets/img/comments_sm.png"/>
+										<a href="#">30 comments</a>
+									</nobr>
 								</div>
 							</div>
 						</div>
@@ -63,7 +80,10 @@
 									<nobr><img src="<?= Yii::app()->baseUrl ?>/assets/img/thumb_up.png"/> 42 votes</nobr>
 								</div>
 								<div class="col-sm-6">
-									<nobr><img src="<?= Yii::app()->baseUrl ?>/assets/img/comments_sm.png"/> 30 comments</nobr>
+									<nobr>
+										<img src="<?= Yii::app()->baseUrl ?>/assets/img/comments_sm.png"/>
+										<a href="#">30 comments</a>
+									</nobr>
 								</div>
 							</div>
 						</div>
@@ -85,7 +105,10 @@
 									<nobr><img src="<?= Yii::app()->baseUrl ?>/assets/img/thumb_up.png"/> 42 votes</nobr>
 								</div>
 								<div class="col-sm-6">
-									<nobr><img src="<?= Yii::app()->baseUrl ?>/assets/img/comments_sm.png"/> 30 comments</nobr>
+									<nobr>
+										<img src="<?= Yii::app()->baseUrl ?>/assets/img/comments_sm.png"/>
+										<a href="#">30 comments</a>
+									</nobr>
 								</div>
 							</div>
 						</div>
