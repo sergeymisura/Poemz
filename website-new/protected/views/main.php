fe0e1b921391a7378d398c33527418e8ce2b37dd
Sergiy Misyura <sergey@cando.com>
2014-09-11 10:13:48 -0400

    Reading FB app id from the config file

--- website-new/protected/views/main.php
+++ website-new/protected/views/main.php
@@ -13,7 +13,7 @@
 		var js, fjs = d.getElementsByTagName(s)[0];
 		if (d.getElementById(id)) return;
 		js = d.createElement(s); js.id = id;
-		js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=565093053580288";
+		js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=<?= Yii::app()->params['fb_app_id'] ?>";
 		fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));</script>
 
