8802df6ea20aea4e1c4c44d4a014b4d9a7a016f6
Sergiy Misyura <sergey@cando.com>
2017-03-09 20:39:35 -0500

    Search API

---
+++ website-new/protected/controllers/api/SearchApiController.php
@@ -0,0 +1,45 @@
+<?php
+class SearchApiController extends ApiController
+{
+	public function actionIndex()
+	{
+		$query = $this->request->getQuery('q', '');
+		if ($query == '')
+		{
+			$this->send(array(
+				'authors' => array(),
+				'poems' => array(),
+				'users' => array()
+			));
+		}
+
+		$authors = Author::model()->findAll(
+			'name like :query',
+			array(
+				':query' => '%' . $query . '%'
+			)
+		);
+
+		$poems = Poem::model()->findAll(
+			'title like :query or first_line like :query',
+			array(
+				':query' => '%' . $query . '%'
+			)
+		);
+
+		$users = User::model()->findAll(
+			'username like :query',
+			array(
+				':query' => '%' . $query . '%'
+			)
+		);
+
+		$this->send(
+			array(
+				'authors' => $authors,
+				'poems' => $poems,
+				'users' => $users,
+			)
+		);
+	}
+}
